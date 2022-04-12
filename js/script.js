3cb22642603ffb1ec451e7f24ca2187be8238216
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-12 02:50:29 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Created by: Zaida Hammel
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
